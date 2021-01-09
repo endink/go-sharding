b0976236ffc9ada2b5897ae24f38d04a54924479
endink <sharping@outlook.com>
2021-01-10 00:40:07 +0800

    support more segment

--- parser/sql_preview.go
+++ parser/sql_preview.go
@@ -5,8 +5,10 @@ import (
 	"unicode"
 )
 
+type StatementType int
+
 const (
-	StmtSelect = iota
+	StmtSelect StatementType = iota
 	StmtStream
 	StmtInsert
 	StmtReplace
@@ -22,18 +24,28 @@ const (
 	StmtOther
 	StmtUnknown
 	StmtComment
+	StmtExplain
+	StmtPriv
+	StmtSavepoint
+	StmtRelease
+	StmtSRollback
 )
 
 // Preview analyzes the beginning of the query using a simpler and faster
 // textual comparison to identify the statement type.
-func PreviewSql(sql string) int {
+func PreviewSql(sql string) StatementType {
 	trimmed := StripLeadingComments(sql)
 
-	firstWord := trimmed
-	if end := strings.IndexFunc(trimmed, unicode.IsSpace); end != -1 {
-		firstWord = trimmed[:end]
+	if strings.Index(trimmed, "/*!") == 0 {
+		return StmtComment
 	}
-	firstWord = strings.TrimLeftFunc(firstWord, func(r rune) bool { return !unicode.IsLetter(r) })
+
+	isNotLetter := func(r rune) bool { return !unicode.IsLetter(r) }
+	firstWord := strings.TrimLeftFunc(trimmed, isNotLetter)
+
+	if end := strings.IndexFunc(firstWord, unicode.IsSpace); end != -1 {
+		firstWord = firstWord[:end]
+	}
 	// Comparison is done in order of priority.
 	loweredFirstWord := strings.ToLower(firstWord)
 	switch loweredFirstWord {
@@ -49,6 +61,8 @@ func PreviewSql(sql string) int {
 		return StmtUpdate
 	case "delete":
 		return StmtDelete
+	case "savepoint":
+		return StmtSavepoint
 	}
 	// For the following statements it is not sufficient to rely
 	// on loweredFirstWord. This is because they are not statements
@@ -73,18 +87,22 @@ func PreviewSql(sql string) int {
 		return StmtShow
 	case "use":
 		return StmtUse
-	case "analyze", "describe", "desc", "explain", "repair", "optimize":
+	case "describe", "desc", "explain":
+		return StmtExplain
+	case "analyze", "repair", "optimize":
 		return StmtOther
-	}
-	if strings.Index(trimmed, "/*!") == 0 {
-		return StmtComment
+	case "grant", "revoke":
+		return StmtPriv
+	case "release":
+		return StmtRelease
+	case "rollback":
+		return StmtSRollback
 	}
 	return StmtUnknown
 }
 
-// StmtType returns the statement type as a string
-func GetStmtType(stmtType int) string {
-	switch stmtType {
+func (s StatementType) String() string {
+	switch s {
 	case StmtSelect:
 		return "SELECT"
 	case StmtStream:
@@ -113,7 +131,25 @@ func GetStmtType(stmtType int) string {
 		return "USE"
 	case StmtOther:
 		return "OTHER"
+	case StmtPriv:
+		return "PRIV"
+	case StmtExplain:
+		return "EXPLAIN"
+	case StmtSavepoint:
+		return "SAVEPOINT"
+	case StmtSRollback:
+		return "SAVEPOINT_ROLLBACK"
+	case StmtRelease:
+		return "RELEASE"
 	default:
 		return "UNKNOWN"
 	}
+}
+
+func (s StatementType) CanHandleWithoutPlan() bool {
+	switch s {
+	case StmtShow, StmtSet, StmtBegin, StmtComment, StmtRollback, StmtUse, StmtPriv, StmtSavepoint, StmtRelease:
+		return true
+	}
+	return false
 }
